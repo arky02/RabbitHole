ef6c1945f9e63af286ba620783baf24591f7c75e
arky02 <kyean07@naver.com>
2024-03-23 15:54:50 +0900

    feat(MyClassButton)

---
+++ src/components/Buttons/MyClassButton.tsx
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+import Image from 'next/image'
+import Class from '/public/icon/class.svg'
+import { COLORS } from '@/styles/palatte'
+
+function MyClassButton({ className }: { className: string }) {
+  return (
+    <StyledButton>
+      <Image src={Class} alt="class" style={{ marginRight: '11px' }} />
+      {className}
+    </StyledButton>
+  )
+}
+export default MyClassButton
+
+const StyledButton = styled.button`
+  padding: 23px 16px;
+  border-radius: 20px;
+  border: 1px solid ${COLORS.GRAY_CD};
+  background: white;
+  color: #666;
+  font-size: 15px;
+  font-weight: 700;
+  display: flex;
+  align-items: center;
+`
