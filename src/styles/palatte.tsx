34ade0fd9abc2c9714b8ecd03abe9ac9b0db066d
arky02 <kyean07@naver.com>
2024-03-23 16:10:26 +0900

    feat(palatte)

---
+++ src/styles/palatte.tsx
@@ -0,0 +1,10 @@
+export const COLORS = {
+  WHITE_FF: '#FFFFFF',
+  GRAY_97: '#979797',
+  GRAY_CD: '#cdcdcd',
+  GRAY_E5: '#E5E5E5',
+  GRAY_D9: '#D9D9D9',
+  GRAY_94: '#949494',
+  MAIN_GRAD: 'linear-gradient(88deg, #D86BFF 0%, #FD7EFF 99.4%)',
+  HILIGHT_GRAD: 'linear-gradient(74deg, #cd3fff 0%, #fc5aff 100%)',
+};
