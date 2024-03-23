e5483178e477cbe6ee4fa5a65d82380a5134a6b7
arky02 <kyean07@naver.com>
2024-03-23 15:49:50 +0900

    feat: mockData 추가

---
+++ src/mockData.ts
@@ -0,0 +1,44 @@
+export const selectClassData = [
+  {
+    school_id: 0,
+    class_no: 11,
+    grade: 6,
+    year: 2024,
+    className: '1학년 영어 A분반',
+  },
+  {
+    school_id: 0,
+    class_no: 11,
+    grade: 6,
+    year: 2024,
+    className: '2학년 영어 B분반',
+  },
+  {
+    school_id: 0,
+    class_no: 11,
+    grade: 6,
+    year: 2024,
+    className: '1학년 수학 A분반',
+  },
+  {
+    school_id: 0,
+    class_no: 11,
+    grade: 6,
+    year: 2024,
+    className: '1학년 과학 A분반',
+  },
+  {
+    school_id: 0,
+    class_no: 11,
+    grade: 6,
+    year: 2024,
+    className: '3학년 영어 B분반',
+  },
+  {
+    school_id: 0,
+    class_no: 11,
+    grade: 6,
+    year: 2024,
+    className: '1학년 영어 C분반',
+  },
+];
