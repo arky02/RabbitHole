c9251b77aef6e1c31cb13310404bc089d2dd2499
arky02 <kyean07@naver.com>
2024-03-26 23:03:21 +0900

    feat: 색 추가

--- src/styles/palatte.tsx
+++ src/styles/palatte.tsx
@@ -4,7 +4,9 @@ export const COLORS = {
   GRAY_CD: '#cdcdcd',
   GRAY_E5: '#E5E5E5',
   GRAY_D9: '#D9D9D9',
+  GRAY_66: '#666',
   GRAY_94: '#949494',
+  RED: '#ED5757',
   MAIN_GRAD: 'linear-gradient(88deg, #D86BFF 0%, #FD7EFF 99.4%)',
   HILIGHT_GRAD: 'linear-gradient(74deg, #cd3fff 0%, #fc5aff 100%)',
 };
