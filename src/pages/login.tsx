92a1d36ec154ff4495185fd3f9c89fce57b6b3d6
arky02 <kyean07@naver.com>
2024-03-28 17:13:18 +0900

    feat: api 연결 및 로그인 상태에 따른 리다이렉션 구현

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -3,22 +3,85 @@ import Input from '@/components/Input';
 import styled from 'styled-components';
 import Image from 'next/image';
 import LogoImg from '/public/icon/logo.svg';
+import { useState } from 'react';
+import { request } from '@/apis/axios';
+import { COLORS } from '@/styles/palatte';
+import toast from 'react-hot-toast';
+import { useRouter } from 'next/router';
+import useManageUserToken from '@/hooks/useManageUserToken';
+import { getAccessTokenFromCookie } from '@/utils/getTokenFromCookie';
+import { isLoggedIn } from '@/utils/validateRedirection';
+import { GetServerSidePropsContext } from 'next';
+
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext,
+) => {
+  return isLoggedIn(await getAccessTokenFromCookie(context))
+    ? {
+        redirect: {
+          destination: '/',
+          permanent: false,
+        },
+      }
+    : {
+        props: {},
+      };
+};
 
 function Login() {
+  const [idText, setIdText] = useState('');
+  const [pwText, setPwText] = useState('');
+  const [errorText, setErrorText] = useState('');
+
+  const router = useRouter();
+  const { saveToken } = useManageUserToken();
+
+  const requestLogin = async () => {
+    try {
+      const loginRes = await request.post('login_teacher', {
+        username: idText,
+        password: pwText,
+      });
+
+      console.log(loginRes.data.token);
+      saveToken(loginRes.data.token);
+      toast.success('로그인 성공');
+      router.push('/');
+    } catch (error) {
+      setErrorText('존재하지 않는 아이디이거나 비밀번호가 잘못되었습니다.');
+    }
+  };
+
   return (
     <PageFlexCenterWrapper>
       <ShadowWrapper>
-        <Image src={LogoImg} width={270} alt="logo"></Image>
+        <Image src={LogoImg} width={270} alt="logo" />
         <FlexColumnWrapper>
           <TextWrapper>
             <LogoText>Log in</LogoText>
             <DescriptionText>- 교사 계정</DescriptionText>
           </TextWrapper>
           <InputWrapper>
-            <Input placeholder="교사 아이디"></Input>
-            <Input placeholder="비밀번호"></Input>
+            <Input
+              placeholder="교사 아이디"
+              text={idText}
+              onChange={(e) => {
+                setIdText(e.target.value!);
+                setErrorText('');
+              }}
+            />
+            <Input
+              placeholder="비밀번호"
+              text={pwText}
+              onChange={(e) => {
+                setPwText(e.target.value!);
+                setErrorText('');
+              }}
+            />
+            {errorText && <ErrorText>{errorText}</ErrorText>}
           </InputWrapper>
-          <Button type="Login" text="로그인" />
+          <Button type="Login" text="로그인" onClick={requestLogin} />
+          <GrayText>도움이 필요하신가요?</GrayText>
         </FlexColumnWrapper>
       </ShadowWrapper>
     </PageFlexCenterWrapper>
@@ -87,4 +150,18 @@ const InputWrapper = styled.div`
   flex-direction: column;
   gap: 12px;
   margin-bottom: 30px;
+`;
+
+const GrayText = styled.h5`
+  color: ${COLORS.GRAY_94};
+  text-align: center;
+  font-size: 14px;
+  margin-top: 20px;
+  font-weight: 500;
+`;
+
+const ErrorText = styled.h5`
+  color: ${COLORS.RED};
+  font-size: 14px;
+  margin-left: 15px;
 `;
