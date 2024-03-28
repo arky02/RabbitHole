bb980ce502b2ee08aabf159dd1e617f2920a5201
arky02 <kyean07@naver.com>
2024-03-28 17:11:32 +0900

    design: 세미콜론 추가

--- src/components/HomeNoticeList.tsx
+++ src/components/HomeNoticeList.tsx
@@ -1,9 +1,9 @@
-import styled from 'styled-components'
-import { TimeDescText } from './TodaysClass'
-import { FlexColumn } from '@/styles/CommonStyles'
-import Image from 'next/image'
-import Separator from '/public/icon/rowStrokeSeparator.svg'
-import { COLORS } from '@/styles/palatte'
+import styled from 'styled-components';
+import { TimeDescText } from './TodaysClass';
+import { FlexColumn } from '@/styles/CommonStyles';
+import Image from 'next/image';
+import Separator from '/public/icon/rowStrokeSeparator.svg';
+import { COLORS } from '@/styles/palatte';
 
 const mockData = [
   ['OOOO 시험 운영 공지', '2024.XX.XX'],
@@ -11,7 +11,7 @@ const mockData = [
   ['OOOO 시험 운영 공지', '2024.XX.XX'],
   ['desfsdfsf', '2024.XX.XX'],
   ['OOOO 시험 운영 공지', '2024.XX.XX'],
-]
+];
 
 function HomeNoticeList() {
   return (
@@ -22,27 +22,27 @@ function HomeNoticeList() {
             <NoticeTitle>{data[0]}</NoticeTitle>
             <TimeDescText>{data[1]}</TimeDescText>
           </RowContainer>
-          <DashedSeparator></DashedSeparator>
+          <DashedSeparator />
         </>
       ))}
     </Wrapper>
-  )
+  );
 }
-export default HomeNoticeList
+export default HomeNoticeList;
 
-const Wrapper = styled(FlexColumn)``
+const Wrapper = styled(FlexColumn)``;
 
 const RowContainer = styled.div`
   display: flex;
   justify-content: space-between;
   margin: 14px 0;
-`
+`;
 const NoticeTitle = styled.h2`
   color: #666;
   font-size: 16px;
   font-weight: 700;
-`
-const DashedSeparator = styled.div`
+`;
+export const DashedSeparator = styled.div`
   border-bottom: 2px dashed ${COLORS.GRAY_E5};
   margin-left: -15px;
-`
+`;
