4cd7ee7ff9811a0f242178bb0536c50109cd53f9
arky02 <kyean07@naver.com>
2024-03-28 17:59:38 +0900

    feat: main 페이지 공지사항 정보 mock data로 분리

--- src/components/HomeNoticeList.tsx
+++ src/components/HomeNoticeList.tsx
@@ -1,22 +1,13 @@
 import styled from 'styled-components';
 import { TimeDescText } from './TodaysClass';
 import { FlexColumn } from '@/styles/CommonStyles';
-import Image from 'next/image';
-import Separator from '@/public/icon/rowStrokeSeparator.svg';
 import { COLORS } from '@/styles/palatte';
-
-const mockData = [
-  ['OOOO 시험 운영 공지', '2024.XX.XX'],
-  ['desfsdfsf', '2024.XX.XX'],
-  ['OOOO 시험 운영 공지', '2024.XX.XX'],
-  ['desfsdfsf', '2024.XX.XX'],
-  ['OOOO 시험 운영 공지', '2024.XX.XX'],
-];
+import { NOTICE } from '@/mockData';
 
 function HomeNoticeList() {
   return (
     <Wrapper>
-      {mockData.map((data, idx) => (
+      {NOTICE.map((data, idx) => (
         <>
           <RowContainer key={idx}>
             <NoticeTitle>{data[0]}</NoticeTitle>
