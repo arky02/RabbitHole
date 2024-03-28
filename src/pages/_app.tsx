d47937c1a8ca431fc0e18be7ba3055db671145c5
arky02 <kyean07@naver.com>
2024-03-28 17:13:00 +0900

    feat: queryClient, react-hot-toast 세팅

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,38 @@
-import type { AppProps } from 'next/app'
-import GlobalStyles from '@/styles/GlobalStyles'
+import type { AppProps } from 'next/app';
+import GlobalStyles from '@/styles/GlobalStyles';
+import { Toaster } from 'react-hot-toast';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useState } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+          },
+        },
+      }),
+  );
   return (
     <>
-      <GlobalStyles />
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <GlobalStyles />
+        <Component {...pageProps} />
+        <Toaster
+          containerStyle={{
+            fontSize: '15px',
+            fontWeight: '600',
+          }}
+          toastOptions={{
+            style: {
+              borderRadius: '999px',
+            },
+            // icon: '🐰',
+          }}
+        />
+      </QueryClientProvider>
     </>
-  )
+  );
 }
