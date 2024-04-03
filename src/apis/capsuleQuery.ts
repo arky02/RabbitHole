577621879281714e7b90b0014ee693ea7cfc430e
arky02 <kyean07@naver.com>
2024-04-04 01:40:58 +0900

    feat: getStudentInfo 쿼리 추가

--- src/apis/capsuleQuery.ts
+++ src/apis/capsuleQuery.ts
@@ -15,7 +15,18 @@ export const getStudentInSession = (accessToken: string, sessionId: number) => {
   return {
     queryKey: ['sessionStudent', sessionId],
     queryFn: () =>
-      request.get(`list_students_in_session?session_id=${sessionId}`, {
+      request.get(`list_students_in_session?session_id=47`, {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      }),
+    enabled: !!accessToken,
+  };
+};
+
+export const getStudentInfo = (accessToken: string, studentUId: number) => {
+  return {
+    queryKey: ['studentInfo', studentUId],
+    queryFn: () =>
+      request.get(`get_student_info?student_uid=${studentUId}`, {
         headers: { Authorization: `Bearer ${accessToken}` },
       }),
     enabled: !!accessToken,
